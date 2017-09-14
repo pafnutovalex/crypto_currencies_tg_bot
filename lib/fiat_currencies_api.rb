5e6c99bd34cb9554222899c33eb9729e5655fbc6
Alex Pafnutov <a.pafnutov@gmail.com>
2017-09-14 13:28:42 +0300

    Fixer API - get fiat currencies rates

---
+++ lib/fiat_currencies_api.rb
@@ -0,0 +1,11 @@
+class FiatCurrenciesApi < CommonApi
+  def initialize()
+  	self.api_url = "https://api.fixer.io/"
+  end
+
+  def get_rates
+  	res = []
+  	response = self.access_url_by('latest', 'get', {base: "USD"})
+  	"USD/RUB: #{response["rates"]["RUB"]}"
+  end
+end
